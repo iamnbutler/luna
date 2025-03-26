e3a2345032e4d9423176c65420a40569f95440df
Nate Butler <iamnbutler@gmail.com>
2025-03-25 22:16:35 -0400

    Start on menus

--- src/luna.rs
+++ src/luna.rs
@@ -1,42 +1,55 @@
 use gpui::{
-    div, prelude::*, rgb, App, Application, FocusHandle, Focusable, Hsla, IntoElement, Window,
-    WindowOptions,
+    actions, div, hsla, prelude::*, App, Application, FocusHandle, Focusable, Hsla, IntoElement,
+    Menu, MenuItem, Window, WindowOptions,
 };
 
-pub const SELECTED_COLOR: Hsla = Hsla {
-    h: 205.0 / 360.0,
-    s: 0.9,
-    l: 0.48,
-    a: 1.0,
-};
+actions!(set_menus, [Quit]);
+
+pub struct Theme {
+    pub background_color: Hsla,
+    pub text_color: Hsla,
+}
+
+impl Theme {
+    pub fn new() -> Self {
+        Theme {
+            background_color: hsla(222.0 / 360.0, 0.12, 0.2, 1.0),
+            text_color: hsla(0.0, 1.0, 1.0, 1.0),
+        }
+    }
+}
 
 struct Luna {
+    theme: Theme,
     focus_handle: FocusHandle,
 }
 
 impl Luna {
     pub fn new(cx: &mut Context<Self>) -> Self {
         let focus_handle = cx.focus_handle();
+        let theme = Theme::new();
 
-        Luna { focus_handle }
+        Luna {
+            focus_handle,
+            theme,
+        }
     }
 }
 
 impl Render for Luna {
     fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         div()
-            .id("luna")
+            .id("Luna")
             .key_context("App")
             .track_focus(&self.focus_handle(cx))
-            .text_xs()
-            .text_color(rgb(0x000000))
-            .font_family("Berkeley Mono")
+            .relative()
+            .size_full()
             .flex()
             .flex_col()
-            .relative()
-            .bg(rgb(0x00FF00))
-            .size_full()
-            .text_color(rgb(0xffffff))
+            .font_family("Berkeley Mono")
+            .text_xs()
+            .bg(self.theme.background_color)
+            .text_color(self.theme.text_color)
     }
 }
 
@@ -48,11 +61,20 @@ impl Focusable for Luna {
 
 fn main() {
     Application::new().run(|cx: &mut App| {
+        cx.activate(true);
+        cx.on_action(quit);
+        cx.set_menus(vec![Menu {
+            name: "Luna".into(),
+            items: vec![MenuItem::action("Quit", Quit)],
+        }]);
+
         cx.open_window(WindowOptions::default(), |_window, cx| {
             cx.new(|cx| Luna::new(cx))
         })
         .unwrap();
-
-        cx.activate(true)
     });
+}
+
+fn quit(_: &Quit, cx: &mut App) {
+    cx.quit();
 }
