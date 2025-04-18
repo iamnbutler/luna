21f30ff85d3ce100df03189d99b87f369c60263c
Nate Butler <iamnbutler@gmail.com>
2025-04-18 09:55:46 -0400

    Add basic input

---
+++ src/input.rs
@@ -0,0 +1,22 @@
+use gpui::actions;
+
+pub mod text_input;
+
+actions!(
+    text_input,
+    [
+        Backspace,
+        Delete,
+        Left,
+        Right,
+        SelectLeft,
+        SelectRight,
+        SelectAll,
+        Home,
+        End,
+        ShowCharacterPalette,
+        Paste,
+        Cut,
+        Copy,
+    ]
+);
--- src/luna.rs
+++ src/luna.rs
@@ -1,3 +1,5 @@
+#![allow(unused, dead_code)]
+
 //! # Luna: A software design tool without compromises.
 //!
 //! Luna is local, files on disk first. Own your own data,
@@ -10,10 +12,13 @@
 //! using an abstractionless design and editing experience.
 
 use gpui::{
-    actions, div, point, prelude::*, px, rgba, App, AppContext, Application, FocusHandle, Menu,
-    MenuItem, Rgba, TitlebarOptions, Window, WindowBackgroundAppearance, WindowOptions,
+    actions, div, point, prelude::*, px, rgba, App, AppContext, Application, Entity, FocusHandle,
+    Focusable, KeyBinding, Keystroke, Menu, MenuItem, MouseButton, MouseUpEvent, Rgba,
+    TitlebarOptions, Window, WindowOptions,
 };
+use input::text_input::TextInput;
 mod geometry;
+mod input;
 
 actions!(luna, [Quit]);
 
@@ -44,12 +49,26 @@ struct Luna {
     // active_canvas: Entity<LunaCanvas>,
     /// Focus handle for keyboard event routing
     focus_handle: FocusHandle,
+    text_input: Entity<TextInput>,
 }
 
 impl Luna {
     fn new(_window: &mut Window, cx: &mut Context<Self>) -> Self {
+        let text_input = cx.new(|cx| TextInput {
+            focus_handle: cx.focus_handle(),
+            content: "".into(),
+            placeholder: "Type here...".into(),
+            selected_range: 0..0,
+            selection_reversed: false,
+            marked_range: None,
+            last_layout: None,
+            last_bounds: None,
+            is_selecting: false,
+        });
+
         Self {
             focus_handle: cx.focus_handle(),
+            text_input,
         }
     }
 
@@ -58,9 +77,16 @@ impl Luna {
     }
 }
 
+impl Focusable for Luna {
+    fn focus_handle(&self, _: &App) -> FocusHandle {
+        self.focus_handle.clone()
+    }
+}
+
 impl Render for Luna {
     fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
         div()
+            .track_focus(&self.focus_handle())
             .items_center()
             .justify_center()
             .id("scene-graph")
@@ -74,7 +100,7 @@ impl Render for Luna {
             .bg(hex("#000000"))
             .size_full()
             .text_color(hex("#FFFFFF"))
-            .child("Luna")
+            .child(self.text_input.clone())
     }
 }
 
@@ -85,6 +111,22 @@ fn main() {
             name: "Luna".into(),
             items: vec![MenuItem::action("Quit", Quit)],
         }]);
+
+        cx.bind_keys([
+            KeyBinding::new("backspace", input::Backspace, None),
+            KeyBinding::new("delete", input::Delete, None),
+            KeyBinding::new("left", input::Left, None),
+            KeyBinding::new("right", input::Right, None),
+            KeyBinding::new("shift-left", input::SelectLeft, None),
+            KeyBinding::new("shift-right", input::SelectRight, None),
+            KeyBinding::new("cmd-a", input::SelectAll, None),
+            KeyBinding::new("cmd-v", input::Paste, None),
+            KeyBinding::new("cmd-c", input::Copy, None),
+            KeyBinding::new("cmd-x", input::Cut, None),
+            KeyBinding::new("home", input::Home, None),
+            KeyBinding::new("end", input::End, None),
+            KeyBinding::new("ctrl-cmd-space", input::ShowCharacterPalette, None),
+        ]);
 
         let window = cx
             .open_window(
@@ -113,6 +155,13 @@ fn main() {
                 cx.activate(true);
             })
             .unwrap();
+
+        // window
+        //     .update(cx, |view, window, cx| {
+        //         window.focus(&view.focus_handle());
+        //         cx.activate(true);
+        //     })
+        //     .unwrap();
     });
 }
 
