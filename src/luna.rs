7c2f3c80de06c942eb07084ec21b900f13fa2b9e
Nate Butler <iamnbutler@gmail.com>
2025-04-20 08:25:03 -0400

    Add basic theme

--- src/luna.rs
+++ src/luna.rs
@@ -11,13 +11,17 @@
 //! That means not just pixels, but representative screens and flows
 //! using an abstractionless design and editing experience.
 
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    ops::{Deref, DerefMut},
+    sync::Arc,
+};
 
 use geometry::LocalPoint;
 use gpui::{
     actions, div, hsla, point, prelude::*, px, rgba, App, AppContext, Application, ElementId,
-    Entity, EventEmitter, FocusHandle, Focusable, KeyBinding, Keystroke, Menu, MenuItem,
-    MouseButton, MouseUpEvent, Rgba, SharedString, TitlebarOptions, WeakEntity, Window,
+    Entity, EventEmitter, FocusHandle, Focusable, Global, Hsla, KeyBinding, Keystroke, Menu,
+    MenuItem, MouseButton, MouseUpEvent, Rgba, SharedString, TitlebarOptions, WeakEntity, Window,
     WindowOptions,
 };
 use input::{InputMap, InputMapKey, TextInput};
@@ -29,6 +33,58 @@ mod sidebar;
 mod typography;
 
 actions!(luna, [Quit]);
+
+#[derive(Clone, Debug)]
+pub struct Theme {
+    fg: Hsla,
+    bg: Hsla,
+    accent: Hsla,
+}
+
+impl Default for Theme {
+    fn default() -> Self {
+        Theme {
+            fg: hsla(220.0 / 360.0, 9.0 / 100.0, 72.0 / 100.0, 1.0),
+            bg: hsla(220.0 / 360.0, 14.0 / 100.0, 18.0 / 100.0, 1.0),
+            accent: hsla(207.0 / 360.0, 82.0 / 100.0, 66.0 / 100.0, 1.0),
+        }
+    }
+}
+
+impl Theme {
+    pub fn get_global(cx: &App) -> &Arc<Theme> {
+        &cx.global::<GlobalTheme>().0
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct GlobalTheme(pub Arc<Theme>);
+
+impl Deref for GlobalTheme {
+    type Target = Arc<Theme>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for GlobalTheme {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Global for GlobalTheme {}
+
+pub trait ActiveTheme {
+    fn theme(&self) -> &Arc<Theme>;
+}
+
+impl ActiveTheme for App {
+    fn theme(&self) -> &Arc<Theme> {
+        &self.global::<GlobalTheme>().0
+    }
+}
 
 struct Luna {
     // The main canvas where elements are rendered and manipulated
@@ -64,7 +120,7 @@ impl Focusable for Luna {
 }
 
 impl Render for Luna {
-    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+    fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         div()
             .relative()
             .track_focus(&self.focus_handle())
@@ -78,9 +134,9 @@ impl Render for Luna {
             .flex()
             .flex_col()
             .relative()
-            .bg(hsla(0.0, 0.0, 0.0, 1.0))
             .size_full()
-            .text_color(hsla(0.0, 1.0, 1.0, 1.0))
+            .text_color(cx.theme().fg)
+            .bg(cx.theme().bg)
             .child(self.sidebar.clone())
     }
 }
@@ -109,6 +165,8 @@ fn main() {
             KeyBinding::new("enter", input::Enter, None),
             KeyBinding::new("ctrl-cmd-space", input::ShowCharacterPalette, None),
         ]);
+
+        cx.set_global(GlobalTheme(Arc::new(Theme::default())));
 
         let window = cx
             .open_window(
